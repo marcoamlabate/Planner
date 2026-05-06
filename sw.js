645d7f41d912e99be0dd4896b31a58976e14a058
marcoamlabate <marcoamlabate@gmail.com>
2026-05-06 18:26:45 -0300

    Update service worker cache for v14

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'adhd-planner-v13-combined';
+const CACHE_NAME = 'adhd-planner-v14-ui-calendar-today';
 const APP_SHELL = [
   './', './index.html', './app.js', './manifest.webmanifest', './icon.png',
   'https://unpkg.com/react@18/umd/react.production.min.js',
