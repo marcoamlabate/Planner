370969fc6af01b243d0e423b085de96f0a2a0f67
marcoamlabate <marcoamlabate@gmail.com>
2026-05-11 15:37:43 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'adhd-planner-v17-save-sync-direct';
+const CACHE_NAME = 'adhd-planner-v18-event-view-fix';
 const APP_SHELL = [
   './', './index.html', './app.js', './manifest.webmanifest', './icon.png',
   'https://unpkg.com/react@18/umd/react.production.min.js',
