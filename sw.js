872be539814f9600c254d8e795d34ed2cef3f878
marcoamlabate <marcoamlabate@gmail.com>
2026-07-10 17:59:35 -0300

    Update Planner v70 cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'adhd-planner-v69-3-utf8-repair';
+const CACHE_NAME = 'adhd-planner-v70-daily-dashboard';
 const APP_SHELL = [
   './', './index.html', './app.js', './manifest.webmanifest', './icon.png',
   'https://unpkg.com/react@18/umd/react.production.min.js',
